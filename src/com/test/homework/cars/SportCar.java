1f45e05d0bbe53a902753076b34dad04f7e4675d
Andrii <avoevodin81@gmail.com>
2022-09-06 22:37:11 +0300

    car task

---
+++ src/com/test/homework/cars/SportCar.java
@@ -0,0 +1,13 @@
+package com.test.homework.cars;
+
+public class SportCar extends Car{
+    public SportCar(Engine engine) {
+        super(engine);
+    }
+
+    @Override
+    public void setEngine() {
+        System.out.println("SportCar engine: ");
+        //implement here
+    }
+}
