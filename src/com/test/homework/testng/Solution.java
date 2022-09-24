56215679f5048d747bf0ed4d06688f7b53733fa6
Andrii <avoevodin81@gmail.com>
2022-09-24 19:01:21 +0300

    testng task

---
+++ src/com/test/homework/testng/Solution.java
@@ -0,0 +1,19 @@
+package com.test.homework.testng;
+
+public class Solution {
+    /*
+    1. Create Maven project
+    2. Into POM.xml add testng dependency
+    3. Into POM.xml add maven surefire plugin into build section
+    https://maven.apache.org/surefire/maven-surefire-plugin/examples/testng.html
+    4. Create two testng xml files - ("smoke.xml", "regression.xml") and put them to src/test/resources/testng/
+    5. Add several tests classes with several test methods and put them into src/test/java/ua/ithillel/ package
+    6. Apply for the test methods "smoke" and "regression" groups
+    7. Add these test cases in a proper testng xml file
+    8. Create "all.xml" testng file and put into there "smoke" and "regression" suites
+    9. Add "all.xml" into maven "suiteXMLFile" section
+    10. Implement one DataProvider with one Test case
+    11. Implement Retry and add it into listener section into testng files (implements IAnnotationTransformer)
+    12. Implement Listener (extends TestListenerAdapter) and add it into listener section into testng files
+     */
+}
